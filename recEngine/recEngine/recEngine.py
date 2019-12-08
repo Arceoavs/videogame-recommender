d1f6b18cd669a59cf44308edcd06dfa9041bf263
luca-d <luca.97.daniel@gmail.com>
2019-12-08 11:31:24 +0100

    added method that takes in user_id and returns recommendations
    Former-commit-id: 42dcb93f39ea7a742d7e2b931891bfaeced73a56

--- recEngine/recEngine/recEngine.py
+++ recEngine/recEngine/recEngine.py
@@ -23,51 +23,37 @@ user_game_matrix = game_user_matrix.T.tocsr()
 model = implicit.als.AlternatingLeastSquares(factors=50)
 model.fit(game_user_matrix)
 
-all_recommendations=model.recommend_all(user_game_matrix)
-print(all_recommendations)
-
-all_recommendations_looped=[]
-for user in range(0, wide_ratings.shape[1]):
-    all_recommendations_looped.append(model.recommend(user, user_game_matrix))
-print(all_recommendations_looped)
-
 games=wide_ratings.index.values.tolist()
 
-simtest=model.similar_items(games.index(5282))
-simgames=[]
-for tup in simtest:
-    simgames.append(games[tup[0]])
-    # TODO: use dictionary to map indices with ids!
-print(simgames)
-#Very good results! Similar games of GTA V based on ratings are:  GTA II, GTA IV, GTA San Andreas, GTA Vice City, ..., Read Dead Redemption, Skyrim
+def recommend(userid):
+    user_ratings_sql = """
+        SELECT
+                user_id,
+                game_id,
+                ROUND(AVG(rating)) rating
+        FROM
+                core.ratings
+        WHERE
+                user_id="""+str(userid)+"""
+        GROUP BY
+                user_id,
+                game_id
+    """
+    user_ratings=sqlio.read_sql(user_ratings_sql, engine)
 
-# sample user profile: GTA IV, GTA V and RDR 2
-# game_ids 5279, 5282, 10158
-sample_profile=[float(0)]*8951
-sample_profile[games.index(5279)]=float(10)
-sample_profile[games.index(5282)]=float(10)
-sample_profile[games.index(10158)]=float(10)
-sample_profile_csr=scipy.sparse.csr_matrix(sample_profile)
-# sample user profile: Battlefield 1, Call of Duty: WWII and Tom Clancys Rainbow Six: Siege
-# game_ids 1301, 2008, 13791
-sample_profile_2=[float(0)]*8951
-sample_profile_2[games.index(1301)]=float(10)
-sample_profile_2[games.index(2008)]=float(10)
-sample_profile_2[games.index(13791)]=float(10)
-sample_profile_2_csr=scipy.sparse.csr_matrix(sample_profile_2)
+    user_profile=[0]*wide_ratings.shape[0]
+    for index, row in user_ratings.iterrows():
+        user_profile[games.index(row['game_id'])] = row['rating']
+    user_profile_csr = scipy.sparse.csr_matrix(user_profile)
 
-rockstar_user_rec=model.recommend(0, sample_profile_csr, recalculate_user=True)
-rockstar_recgames=[]
-for tup in rockstar_user_rec:
-    rockstar_recgames.append(games[tup[0]])
-    #TODO: use dictionary to map indices with ids!
-print(rockstar_recgames)
+    user_rec = model.recommend(0, user_profile_csr, recalculate_user=True)
+    user_recgames = []
+    for tup in user_rec:
+        user_recgames.append((games[tup[0]], tup[1]))
 
-shooter_user_rec=model.recommend(0, sample_profile_2_csr, recalculate_user=True)
-shooter_recgames=[]
-for tup in shooter_user_rec:
-    shooter_recgames.append(games[tup[0]])
-    #TODO: use dictionary to map indices with ids!
-print(shooter_recgames)
+    return user_recgames
+
+print(recommend(1))
+print(recommend(2))
 
 
