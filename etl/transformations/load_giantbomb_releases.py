87f13e5485500795d45da6bc4cdb641b91ca241e
David Wilhelm <davidwlhlm@icloud.com>
2019-12-18 10:54:16 +0100

    Fix data type in giantbomb releases
    Former-commit-id: 65c825b124b4024429058468caae78827d528e4a

--- etl/transformations/load_giantbomb_releases.py
+++ etl/transformations/load_giantbomb_releases.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import sqlalchemy
+from sqlalchemy.dialects import postgresql
 from utilities import engine
 
 file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../datasets/giantbomb/releases.csv'))
@@ -11,6 +13,6 @@ df = pd.read_csv(file_path, sep=';', quotechar='|')
 # Drop guid and game_name column
 df = df.drop(['guid', 'game_name'], axis=1)
 
-print(df.dtypes)
-
-df.to_sql('stage_releases', engine, schema='giantbomb', if_exists='replace', index=False)
+df.to_sql('stage_releases', engine, schema='giantbomb', if_exists='replace', index=False, dtype={
+  'release_date': sqlalchemy.types.TIMESTAMP,
+  })
