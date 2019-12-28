759ea2c59b81372c4e6b9e9c4d02376d037348f2
Arnold Arz <arnoarz@Arnolds-MacBook-Pro.local>
2019-12-28 01:36:44 +0100

    :lipstick: hiding social icons on xs
    Former-commit-id: 1bd142f91949b657eeb86a0f22ab177ed847247c

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,42 +2,42 @@
 v-footer(padless elevation="2")
   v-card.flex(flat tile color="dp1")
     v-card-title
-      .hidden-sm-and-down
+      #buttons.hidden-sm-and-down
         v-btn(text color="primary_var") {{"home"}}
         v-btn(text color="primary_var") {{"about"}}
-        v-btn(text color="primary_var") {{"team"}}
         v-btn(text color="primary_var") {{"contact"}}
       v-spacer
 
-      v-tooltip(top)
-        template(v-slot:activator="{on}")
-          v-btn.mx-4(dark icon v-on="on")
-            v-icon(color="primary_var") mdi-facebook
-        span Facebook
+      #social.hidden-xs-only
+        v-tooltip(top)
+          template(v-slot:activator="{on}")
+            v-btn.mx-4(dark icon v-on="on")
+              v-icon(color="primary_var") mdi-facebook
+          span Facebook
 
-      v-tooltip(top)
-        template(v-slot:activator="{on}")
-          v-btn.mx-4(dark icon v-on="on")
-            v-icon(color="primary_var") mdi-twitter
-        span Twitter
+        v-tooltip(top)
+          template(v-slot:activator="{on}")
+            v-btn.mx-4(dark icon v-on="on")
+              v-icon(color="primary_var") mdi-twitter
+          span Twitter
 
-      v-tooltip(top)
-        template(v-slot:activator="{on}")
-          v-btn.mx-4(dark icon v-on="on")
-            v-icon(color="primary_var") mdi-linkedin
-        span LindedIn
+        v-tooltip(top)
+          template(v-slot:activator="{on}")
+            v-btn.mx-4(dark icon v-on="on")
+              v-icon(color="primary_var") mdi-linkedin
+          span LindedIn
 
-      v-tooltip(top)
-        template(v-slot:activator="{on}")
-          v-btn.mx-4(dark icon v-on="on")
-            v-icon(color="primary_var") mdi-instagram
-        span Instagram
+        v-tooltip(top)
+          template(v-slot:activator="{on}")
+            v-btn.mx-4(dark icon v-on="on")
+              v-icon(color="primary_var") mdi-instagram
+          span Instagram
 
-      v-tooltip(top)
-        template(v-slot:activator="{on}")
-          v-btn.mx-4(dark icon v-on="on")
-            v-icon(color="primary_var") mdi-youtube
-        span Youtube
+        v-tooltip(top)
+          template(v-slot:activator="{on}")
+            v-btn.mx-4(dark icon v-on="on")
+              v-icon(color="primary_var") mdi-youtube
+          span Youtube
 
     v-divider
     v-card-text.py-2.white--textarea.text-center
