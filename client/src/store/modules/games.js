2dd2cff58f4751ed46a501d1b4bf6a790f3282ff
Arnold Arz <arnoarz@gmail.com>
2020-01-13 12:05:22 +0100

    :recycle: swiper component and rated games
    Former-commit-id: a089f2c56388699fb105d3eccca3210bc2d62079

--- client/src/store/modules/games.js
+++ client/src/store/modules/games.js
@@ -9,16 +9,26 @@ function concanateSting ({ offset, limit }) {
   }
   return url
 }
+
+async function retrieveGame (id) {
+  try {
+    const res = await Vue.prototype.$http.get('/games/' + id)
+    return res.data.game
+  } catch (err) {
+  }
+}
+
 export default {
   state: {
-    games: []
+    games: [],
+    ratedGames: []
   },
   mutations: {
     resetGames (state) {
       state.games = []
     },
-    setGames (state, games) {
-      state.games = games
+    setRatedGames (state, games) {
+      state.ratedGames = games
     },
     addGames (state, games) {
       // All games that are not already in the list
@@ -45,6 +55,15 @@ export default {
     async loadPrev ({ dispatch, getters }) {
       await dispatch('retrieveGames',
         { offset: getters.range.min - 2, limit: 1 })
+    },
+    async retrieveGamesRatedByUser ({ commit, rootState, dispatch }) {
+      await dispatch('retrieveUserData')
+      const ratedGames = []
+      rootState.userData.userData.ratings.forEach(async rating => {
+        const game = await retrieveGame(rating.game_id)
+        ratedGames.push(game)
+      })
+      commit('setRatedGames', ratedGames)
     }
   },
   getters: {
@@ -53,6 +72,7 @@ export default {
     range: state => {
       return { max: Math.max(...state.games.map(o => o.id), 0),
         min: Math.min(...state.games.map(o => o.id), Number.MAX_SAFE_INTEGER) }
-    }
+    },
+    ratedGames: state => state.ratedGames
   }
 }
--- client/src/views/Rate.vue
+++ client/src/views/Rate.vue
@@ -1,16 +1,15 @@
 <template lang="pug">
-v-container(fluid)
+v-container
   v-row(justify="center" align="start")
-    v-col(cols="10")
+    v-col(cols="12")
       h1.display-2.font-weight-bold Ratings
 
   v-row(justify="center" align="start")
-    v-col(cols="10")
+    v-col(cols="12")
       h2.display-1 Rate new games
 
   v-row(justify="center" align="start")
-    v-col(cols="1")
-    v-col(cols="5")
+    v-col(cols="6")
       v-autocomplete(outlined
         dense
         prepend-inner-icon="mdi-gamepad-variant-outline"
@@ -23,83 +22,72 @@ v-container(fluid)
         multiple
         placeholder="Filter genres")
 
-    v-col(cols="5")
+    v-col(cols="6")
       v-text-field(outlined
         dense
         clearable
         prepend-inner-icon="mdi-magnify"
         placeholder="Search for games")
-    v-col(cols="1")
 
   v-row(justify="start" align="center")
-    v-col.hide-sm-and-down(cols="1"
-      align="right")
-      v-icon.prev-button(size="50")
-        | mdi-chevron-left
-
-    v-col(cols="10")
-      swiper(:options="swiperOptions"
-        ref="swiper"
+    v-col(cols="12")
+      Swiper(ref="swiperNewRatings"
         @reachEnd="$store.dispatch('loadNext')"
-        @reachBeginning="$store.dispatch('loadPrev')")
-        swiper-slide(v-for="game in sortedGames"
-          :key="game.id")
-          GameCard(:game="game")
+        @reachBeginning="$store.dispatch('loadPrev')"
+        :slidesPerView="3"
+        :spaceBetween="30"
+        :games="sortedGames")
 
-    v-col.hide-sm-and-down(cols="1"
-      align="left")
-      v-icon.next-button(size="50")
-        | mdi-chevron-right
+  v-row(justify="start" align="center")
+    v-col(align="center")
+      v-icon mdi-chevron-left
+    v-col(align="center")
+      p.font-weight-light.font-italic
+        | Swipe left and right
+    v-col(align="center")
+      v-icon mdi-chevron-right
 
+  v-row.mt-2(justify="center" align="start")
+    v-col(cols="12")
+      h2.display-1 Your ratings
+
+  v-row(justify="start" align="center")
+    v-col(cols="12")
+      Swiper(ref="swiperOldRatings"
+        :slidesPerView="3"
+        :spaceBetween="30"
+        :games="ratedGames")
+
+  v-row(justify="start" align="center")
+    v-col(align="center")
+      v-icon mdi-chevron-left
+    v-col(align="center")
+      p.font-weight-light.font-italic
+        | Swipe left and right
+    v-col(align="center")
+      v-icon mdi-chevron-right
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
-import GameCard from '@/components/games/GameCard'
+import Swiper from '@/components/games/Swiper'
 
 export default {
   name: 'Ratings',
   components: {
-    swiper,
-    swiperSlide,
-    GameCard
+    Swiper
   },
   data () {
     return {
-      swiperOptions: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: '.next-button',
-          prevEl: '.prev-button'
-        },
-        breakpoints: {
-          1264: {
-            slidesPerView: 3,
-            spaceBetween: 30
-          },
-          960: {
-            slidesPerView: 2,
-            spaceBetween: 20
-          },
-          600: {
-            slidesPerView: 1,
-            spaceBetween: 10
-          }
-        }
-      },
-      selectedGenres: []
-    }
+      selectedGenres: [] }
   },
   computed: {
-    ...mapGetters(['sortedGames', 'range', 'sortedGenres'])
-
+    ...mapGetters(['sortedGames', 'range', 'sortedGenres', 'ratedGames', 'ratings'])
   },
   mounted () {
     this.$store.dispatch('retrieveGames', { limit: 10 })
     this.$store.dispatch('retrieveGenres')
+    this.$store.dispatch('retrieveGamesRatedByUser')
   }
 }
 </script>
