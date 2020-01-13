cf72295f91b3e1456642589550e49c0abac54eca
Arnold Arz <arnoarz@gmail.com>
2020-01-13 17:43:17 +0100

    :bento: adding background for recommendations
    Former-commit-id: 9e4d0d70d7765c3814ca1a3b048bebfabc011e57

--- client/src/views/Recommend.vue
+++ client/src/views/Recommend.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
-v-container
-  v-row(justify="center" align="start")
-    v-col(cols="12")
-      h1.display-2.font-weight-bold
-        span Recommendations
-        span(v-if="")
+v-img(:aspect-ratio="4/3"
+  src="@/assets/keyboard.jpg"
+  gradient="0.5turn, rgba(18, 18, 18, 1) 50%, rgba(0,0,0,0)")
+  v-container
+    v-row(justify="center" align="start")
+      v-col(cols="12")
+        h1.display-2.font-weight-bold
+          | Recommend
 
-  OnboardingFirst#OnboardingNeeded(v-if="onboarding")
+    OnboardingFirst#OnboardingNeeded(v-if="onboarding")
 
-  v-row#OnboardingDone.mt-2(v-else justify="center" align="start")
-    v-col(cols="12")
-      h2.display-1
-        | The games you might want to try
+    v-row#OnboardingDone.mt-2(v-else justify="center" align="start")
+      v-col(cols="12")
+        h2.display-1
+          | The games you might want to try
 </template>
 
 <script>
