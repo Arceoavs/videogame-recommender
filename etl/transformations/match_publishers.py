b33c366ba4839b31e1633b99809c841bd0585fc2
csab <csabeel@gmail.com>
2020-01-15 05:23:38 +0100

    enhanced and refactored publishers matching in match_publishers.py
    Former-commit-id: b4216c9de7f34ee07d9227fb8538f0f2bff7d23d

--- etl/transformations/match_publishers.py
+++ etl/transformations/match_publishers.py
@@ -8,12 +8,7 @@ from pandas import DataFrame
 from utilities import engine
 
 with engine.connect() as connection:
-    # giantBomb_publishers = pd.read_sql_query(
-    #     '''
-    #     SELECT description regexp_matches('published by\s+\K\S+ ') FROM giantbomb.stage_games;
-    #     ''',
-    #     connection
-    # )
+
 
     igdb_publishers = pd.read_sql_query(
         ''' 
@@ -40,15 +35,14 @@ with engine.connect() as connection:
         igdb_publishers, metacritic_publishers,
         'id', 'id',
         'publisher', 'publisher',
-        0,
-        #TODO: Evaluate if treshold of 0 makes sense - this is equivalent to a normal join on the publisher names, it doesn't match any publishers with different names
+        1,
         l_out_attrs=['id', 'publisher'], r_out_attrs=['id', 'publisher'],
         l_out_prefix='igdb_', r_out_prefix='metacritic_'
     )
 
     matching_publishers.to_sql('publishers', engine, schema='matching', if_exists='replace', index=False)
 
-# renaming the columns to similar and dissimilar names in both dataframes
+    # renaming the columns to similar and dissimilar names in both dataframes
 
     # oriented on  match_platforms:
     metacritic_merged = matching_publishers.merge(
@@ -86,27 +80,7 @@ with engine.connect() as connection:
     })
 
 
-    #print(metacritic_merged)
-
-    # integrated_publishers = pd.concat([metacritic_publishers, igdb_publishers], join="outer",
-    #                                   ignore_index=True, keys=None, levels=None, names=None, verify_integrity=False,
-    #                                   sort=True, copy=True)
-    #
-    # integrated_publishers.to_sql('integrated_publishers', engine, schema='public', if_exists='replace', index=False)
-
-    # connection.execute(
-    #     """
-    #     ALTER TABLE integrated_publishers ADD PRIMARY KEY (ID);
-    #     """
-    #     )
-
 
     connection.close()
 
-
-    # print(tabulate(igdb_publishers.head(10)))
-    # print(tabulate(matching_publishers.head(10)))
-    # print(matching_publishers)
-    # print("integrated publichers")
-    # print(tabulate(integrated_publishers.head(10)))
 
