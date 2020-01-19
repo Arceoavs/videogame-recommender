fc681b4616b1d78120da335481dbc91f4daea4d2
David Wilhelm <davidwlhlm@icloud.com>
2020-01-19 17:28:15 +0100

    Refine game matching and deactivate platform filter
    Former-commit-id: 850da5d54ba0470247c6acaecabe46e24d1a288e

--- etl/transformations/queries/select_igdb_games.py
+++ etl/transformations/queries/select_igdb_games.py
@@ -1,29 +1,27 @@
 import pandas as pd
 
+
 def select_igdb_games(connection):
-  return pd.read_sql_query(
-    ''' 
-    SELECT 
-      g.id,
-      title,
-      year,
-      string_agg(p.name, ', ') as platforms
-    FROM (
-      SELECT *, unnest(platforms) as platform
-      FROM (
-        SELECT 
-          g.id,
-          trim(unnest(array_append(a.alternative_names, g.name))) as title,
-          EXTRACT('YEAR' FROM g.first_release_date) as year,
-          g.platforms
-        FROM igdb.stage_games g
+    return pd.read_sql_query(
+        ''' 
+        SELECT
+          game.id,
+          trim(unnest(array_append(a.alternative_names, game.name))) as title,
+          EXTRACT('YEAR' FROM min(first_release_date)) as year,
+          STRING_AGG(DISTINCT p.platform_name, ', ') as platforms
+        FROM igdb.stage_games game 
+        -- alternative names
         LEFT JOIN (
           SELECT game as game_id, array_agg(name) as alternative_names FROM igdb.stage_alternative_names GROUP BY 1
-        ) a ON g.id = a.game_id
-      ) g
-    ) g
-    LEFT JOIN lookup.platforms p on g.platform = p.igdb_id
-    GROUP BY 1,2,3;
-    ''',
-    connection
-  )
+        ) a ON game.id = a.game_id
+        -- platforms
+        LEFT JOIN (
+          SELECT game_id, name as platform_name FROM (
+          SELECT id AS game_id, unnest(platforms) AS platform_id FROM igdb.stage_games
+          ) op
+          LEFT JOIN lookup.platforms lp ON lp.igdb_id = op.platform_id
+        ) p on p.game_id = game.id
+        GROUP BY 1,2;
+        ''',
+        connection
+    )
