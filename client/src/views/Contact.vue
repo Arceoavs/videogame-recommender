c2e8ccd49d8805e10cae39ace90118383c256b61
Arnold Arz <arnoarz@gmail.com>
2020-01-19 02:02:40 +0100

    :recycle: new contact page structure
    Former-commit-id: 9ccacc1a23018260daeef84cc63e599fba968c84

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -11,104 +11,68 @@ v-container
           | Who is behind the Video Game Recommender?
 
   v-row(justify="space-around")
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
-
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
-
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
-
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
-
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
-
-    v-col(cols="12" lg="4" md="6" sm="6")
-      v-card(color="dp1")
-        v-img.white--text.align-end(height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg")
-          v-card-title
-            | Arnold Arz von Straussenburg
-        v-card-subtitle.overline
-          | Frontend Developer
-        v-card-actions
-          v-btn(text
-            color="primary"
-            small
-            href="mailto:arnoarz@gmail.com")
-            v-icon.mr-2 mdi-email
-            | arnold@vgr.best
+    v-col(cols="12" lg="4" md="6" sm="6"
+      v-for="dude in team"
+      :key="dude.name")
+      v-hover(v-slot:default="{ hover }")
+        v-card(color="dp1"
+          :elevation="hover ? 12 : 1")
+          v-img.white--text.align-end(height="200px"
+            :src="dude.image")
+            v-card-title
+              | {{dude.name}}
+          v-card-subtitle.overline
+            | {{dude.role}}
+          v-card-actions
+            v-btn(text
+              color="primary"
+              small
+              :href="mailLink(dude.mail)")
+              v-icon.mr-2 mdi-email
+              | {{dude.mail}}
   v-row
     v-col
       p.font-weight-light.title
         | For suggestions and feedback, feel free to send us a mail!
 
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      team: [
+        { name: 'Luca Daniel',
+          role: 'Data Integrator',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+        { name: 'Arnold Arz von Straussenburg',
+          role: 'Frontend Developer',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+        { name: 'David Wilhelm',
+          role: 'Backend Developer',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+        { name: 'Arnold Arz von Straussenburg',
+          role: 'Frontend Developer',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+        { name: 'Arnold Arz von Straussenburg',
+          role: 'Frontend Developer',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+        { name: 'Arnold Arz von Straussenburg',
+          role: 'Frontend Developer',
+          mail: 'arnold@vgr.best',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' }
+      ]
+    }
+  },
+  methods: {
+    mailLink (address) {
+      return 'mailto:' + address
+    }
+  }
+}
+</script>
