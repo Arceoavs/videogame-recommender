1756bbb655defe4ce883010b277fe8fdcb117081
Arnold Arz <arnoarz@gmail.com>
2020-01-23 17:05:20 +0100

    :lipstick: image full height and added description
    Former-commit-id: 5f3870cc0156c7caf7bb63f3ec8dda75d9b6f0ff

--- client/src/views/Recommend.vue
+++ client/src/views/Recommend.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-v-img(:aspect-ratio="4/3"
+v-img(height="100%"
   src="@/assets/keyboard.jpg"
   gradient="0.5turn, rgba(18, 18, 18, 1) 50%, rgba(0,0,0,0)")
   v-container
     v-row(justify="center" align="start")
       v-col(cols="12")
         h1.display-2.font-weight-bold
-          | Recommend
+          | Recommendations
 
     OnboardingFirst#OnboardingNeeded(v-if="onboarding")
 
@@ -16,6 +16,8 @@ v-img(:aspect-ratio="4/3"
           v-col(cols="12" sm="8")
             h2.display-1
               | The games you might want to try
+            p.mt-1.font-weight-light.title
+              | If you already habe played a game of the list, just give it a rating.
           v-col(cols="6" sm="4" align="end")
             v-btn(@click="reretrieve"
               color="primary"
