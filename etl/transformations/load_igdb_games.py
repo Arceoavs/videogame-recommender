d299d1ea0b637c15d264c6c55eefc389db1b8fc3
David Wilhelm <davidwlhlm@icloud.com>
2020-01-27 17:02:15 +0100

    Improve loading
    Former-commit-id: 8d286e2d6e4daaf3091a5b72dce3576a533f276e

--- etl/transformations/load_igdb_games.py
+++ etl/transformations/load_igdb_games.py
@@ -16,32 +16,21 @@ df = pd.read_csv(file_path, sep=';', quotechar='|')
 # Convert numeric timestamp to date
 df['first_release_date'] = pd.to_datetime(df.first_release_date, unit='s')
 
-# Convert some list strings in lists
 df['alternative_names'] = df.alternative_names.apply(liststringToList)
-df['franchises'] = df.franchises.apply(liststringToList)
 df['genres'] = df.genres.apply(liststringToList)
-df['involved_companies'] = df.involved_companies.apply(liststringToList)
-df['keywords'] = df.keywords.apply(liststringToList)
 df['platforms'] = df.platforms.apply(liststringToList)
 df['release_dates'] = df.release_dates.apply(liststringToList)
-df['similar_games'] = df.similar_games.apply(liststringToList)
-df['themes'] = df.themes.apply(liststringToList)
 
 # Drop some columns
-df = df.drop(['bundles', 'dlcs', 'expansions', 'franchise', 'game_engines', 'popularity', 'rating', 'aggregated_rating', 'total_rating'], axis=1)
+df = df.drop(['bundles', 'dlcs', 'expansions', 'franchise', 'franchises', 'game_engines', 'keywords', 'involved_companies', 'popularity', 'rating', 'similar_games', 'aggregated_rating', 'total_rating', 'themes'], axis=1)
 
 # dtypes are only needed, if pandas type inference is wrong and typing with pandas is too difficult!
 df.to_sql(f'stage_{file_name}', engine, schema=file_dir, if_exists='replace', index=False, dtype={
     'first_release_date': sqlalchemy.types.TIMESTAMP,
     'parent_game': sqlalchemy.types.INT,
     'version_parent': sqlalchemy.types.INT,
-    'alternative_names': postgresql.ARRAY(sqlalchemy.types.INT),
-    'franchises': postgresql.ARRAY(sqlalchemy.types.INT),
+    'alternative_names': postgresql.ARRAY(sqlalchemy.types.INT), 
     'genres': postgresql.ARRAY(sqlalchemy.types.INT),
-    'involved_companies': postgresql.ARRAY(sqlalchemy.types.INT),
-    'keywords': postgresql.ARRAY(sqlalchemy.types.INT),
     'platforms': postgresql.ARRAY(sqlalchemy.types.INT),
     'release_dates': postgresql.ARRAY(sqlalchemy.types.INT),
-    'similar_games': postgresql.ARRAY(sqlalchemy.types.INT),
-    'themes': postgresql.ARRAY(sqlalchemy.types.INT),
   })
